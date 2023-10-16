446f3cd750563498df2e86ebf14f0d7088d652b1
Arman Ommid <armanommid@gmail.com>
2023-10-16 00:22:44 -0700

    more reuquirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+
+class AppendPaths(Command):
+    def run(self):
+        import sys, os
+        DIR = os.getcwd()
+        sys.path.append(os.path.join(DIR, "src", "taming-transformers"))
+        sys.path.append(os.path.join(DIR, "src", "clip"))
 
 setup(
     name='latent-diffusion',
